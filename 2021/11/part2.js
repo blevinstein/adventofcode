f2102c0040cfae5810f2cb169ed7bc825e5230eb
Brian Levinstein <blevinstein@gmail.com>
2021-12-13 10:42:34 -0500

    Complete day 11

---
+++ 2021/11/part2.js
@@ -0,0 +1,90 @@
+
+const fs = require('fs');
+
+const OFFSETS = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, -1]
+];
+
+const DEBUG = false;
+
+function neighbors(r, c) {
+  return OFFSETS
+      .map(offset => [r + offset[0], c + offset[1]])
+      .filter(position => 0 <= position[0] && position[0] < 10
+          && 0 <= position[1] && position[1] < 10);
+}
+
+function key(r, c) {
+  return `${r},${c}`;
+}
+
+function coords(key) {
+  return key.split(',').map(s => parseInt(s));
+}
+
+async function main() {
+  const state = fs.readFileSync(process.argv[2])
+      .toString()
+      .trim()
+      .split('\n')
+      .map(row => Array.from(row).map(s => parseInt(s)));
+
+  let flashes = 0;
+  for (let step = 0; true; ++step) {
+    // Increment
+    for (let i = 0; i < 10; ++i) {
+      for (let j = 0; j < 10; ++j) {
+        state[i][j]++;
+      }
+    }
+    // Flash
+    const flashed = new Set;
+    for (let i = 0; i < 10; ++i) {
+      for (let j = 0; j < 10; ++j) {
+        // Flood
+        const stack = [[i, j]];
+        while (stack.length > 0) {
+          const [r, c] = stack.pop();
+          if (state[r][c] >= 10 && !flashed.has(key(r, c))) {
+            //console.log(`flash ${r},${c}`);
+            flashed.add(key(r, c));
+            for (let neighbor of neighbors(r, c)) {
+              state[neighbor[0]][neighbor[1]]++;
+              stack.push(neighbor);
+            }
+          }
+        }
+      }
+    }
+    if (flashed.size == 100) {
+      console.log(`All flash at step ${step + 1}`);
+      break;
+    }
+    // Clear
+    flashes += flashed.size;
+    for (let flash of flashed) {
+      const [i, j] = coords(flash);
+      state[i][j] = 0;
+    }
+    if (DEBUG) {
+      for (let i = 0; i < 10; ++i) {
+        for (let j = 0; j < 10; ++j) {
+          process.stdout.write(`${state[i][j]}`);
+        }
+        process.stdout.write('\n');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  console.log(`Flashes=${flashes}`);
+}
+
+main();

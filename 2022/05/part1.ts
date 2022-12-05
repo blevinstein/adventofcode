b865923a0da19a84a6c306f8c98db2a92e613ca0
Brian Levinstein <blevinstein@gmail.com>
2022-12-05 10:12:25 -0700

    Solve day 5

---
+++ 2022/05/part1.ts
@@ -0,0 +1,45 @@
+
+import fs from 'fs';
+
+function* range(n, start = 0, step = 1): Iterator<number> {
+  for (let i = start; i < n + start; i += step) yield i;
+}
+
+async function main() {
+  const rawInput: string = fs.readFileSync(process.argv[2]).toString();
+  const [cratesInput, movesInput] = rawInput.split('\n\n');
+  const cratesLines = cratesInput.split('\n');
+  const maxItems = cratesLines.length - 1;
+  const labelLine = cratesLines[maxItems];
+  const numStacks = (cratesLines[0].length + 1)/4;
+  const cratesGrid = cratesLines.map(line =>
+      Array.from(range(numStacks)).map(i => line[i * 4 + 1]));
+  //console.log(cratesGrid);
+  const labels =
+    Array.from(range(numStacks)).map(i => labelLine[i * 4 + 1]);
+  let stacks = new Map(labels.map(label => [label, []]));
+  for (let row = maxItems - 1; row >= 0; row--) {
+    for (let stack = 0; stack < numStacks; stack++) {
+      if (cratesGrid[row][stack] != ' ') {
+        const label = labelLine[stack * 4 + 1];
+        stacks.get(label).push(cratesGrid[row][stack]);
+      }
+    }
+  }
+  //console.log(stacks);
+  for (const line of movesInput.trim().split('\n')) {
+    //console.log(line);
+    const [_, numString, source, dest] = line.match(/move (\d+) from (\d+) to (\d+)/);
+    const num = Number(numString);
+    for (let i = 0; i < num; ++i) {
+      stacks.get(dest).push(stacks.get(source).pop());
+    }
+    //console.log(stacks);
+  }
+  console.log(
+      Array.from(range(numStacks)).map(i => stacks.get(labelLine[i * 4 + 1]).pop()).join(''));
+}
+
+main()
+  .then(() => console.log('Done'))
+  .catch(e => console.error(e));

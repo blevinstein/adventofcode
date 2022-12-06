4ca91ef8296d7cf177f783786f5c8071c1111f4f
Brian Levinstein <blevinstein@gmail.com>
2022-12-05 22:45:12 -0700

    Solve day 6

---
+++ 2022/06/part1.ts
@@ -0,0 +1,25 @@
+
+import fs from 'fs';
+
+function* range(n, start = 0, step = 1): Iterator<number> {
+  for (let i = 0; i < n; i ++) {
+    yield (i * step + start);
+  }
+}
+
+async function main() {
+  const rawInput: string = fs.readFileSync(process.argv[2]).toString().trim();
+  const windowSize = 4;
+  const result = Array.from(range(rawInput.length)).map(i => {
+    const window = rawInput.slice(i < windowSize ? 0 : i - windowSize, i)
+    return [i, new Set(window).size];
+  }).find((args) => {
+    const [i, count] = args;
+    return count == windowSize;
+  });
+  console.log(result);
+}
+
+main()
+  .then(() => console.log('Done'))
+  .catch(e => console.error(e));

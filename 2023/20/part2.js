cd70e472af5b157e9870250c681ead754595502f
Brian Levinstein <blevinstein@gmail.com>
2023-12-20 01:40:43 -0500

    Solve day 20

--- 2023/20/part2.js
+++ 2023/20/part2.js
@@ -19,6 +19,25 @@ async function main() {
         return [ name, { type, destinations: destinations.split(', ') }];
       }));
 
+  // From manual inspection:
+  // cn -> -> &dr -> &mr(inverter) -> &qb -> rx
+  // xk -> -> &sv -> &rz(inverter) -> &qb -> rx
+  // rj -> -> &qz -> &kv(inverter) -> &qb -> rx
+  // gf -> -> &sk -> &jg(inverter) -> &qb -> rx
+  //
+  // So if dr/sv/qz/sk all emit on different cycles, we can combine to know when qb will be
+  // triggered. We look for a low pulse to the inverter (not the combinator), by analogy to
+  // broadcaster/rx.
+  const counts = [
+    pressUntilLowPulse(modules, 'cn', 'mr'),
+    pressUntilLowPulse(modules, 'xk', 'rz'),
+    pressUntilLowPulse(modules, 'rj', 'kv'),
+    pressUntilLowPulse(modules, 'gf', 'jg'),
+  ];
+  console.log({ counts, product: counts.reduce((a, b) => a * b) });
+}
+
+function pressUntilLowPulse(modules, initialTarget, pulseReceiver) {
   const memory = {};
   for (let [ name, module ] of Object.entries(modules)) {
     if (module.type === 'broadcaster') continue;
@@ -32,7 +51,6 @@ async function main() {
               .map(([newName, newModule]) => [newName, false]));
     }
   }
-  console.log({ modules, memory });
 
   let done = false;
   let buttonPresses = 0;
@@ -40,16 +58,16 @@ async function main() {
   while (!done) {
     buttonPresses++;
 
-    //if (buttonPresses % 1e5 === 0) console.log(buttonPresses);
-    console.log('press button');
+    // Progress monitoring
+    if (buttonPresses % 1e5 === 0) console.log(JSON.stringify(memory, null, 4));
 
-    const pulses = [['broadcaster', false, null]];
+    const pulses = [[initialTarget, false, null]];
     while (pulses.length > 0) {
       const [ address, level, source ] = pulses.shift();
 
       //console.log([ address, level, source ]);
 
-      if (address === 'rx' && !level) done = true;
+      if (address === pulseReceiver && !level) done = true;
 
       // Eat signals to untyped modules
       if (!(address in modules)) continue;
@@ -63,14 +81,14 @@ async function main() {
         // Respond to low pulses only
         if (!level) {
           memory[address] = !memory[address];
-          console.log(`set ${address} = ${memory[address]}`);
+          //console.log(`set ${address} = ${memory[address]}`);
           for (let destination of destinations) {
             pulses.push([ destination, memory[address], address ]);
           }
         }
       } else if (type === '&') {
         memory[address][source] = level;
-        console.log(`remember ${source} -> ${address} = ${level}`);
+        //console.log(`remember ${source} -> ${address} = ${level}`);
         const outLevel = !Object.values(memory[address]).every(v => v);
         for (let destination of destinations) {
           pulses.push([ destination, outLevel, address ]);
@@ -79,7 +97,7 @@ async function main() {
     }
   }
 
-  console.log({ buttonPresses });
+  return buttonPresses;
 }
 
 main()

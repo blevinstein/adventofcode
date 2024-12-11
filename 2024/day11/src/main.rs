4516f452e8f159204d78fe22affaf0013c0733b6
Brian Levinstein <blevinstein@gmail.com>
2024-12-10 23:07:27 -0700

    Fix part 1. Solve part 2.

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::collections::HashMap;
 
 fn count_digits(stone: &usize) -> usize {
     let mut digits = 0u32;
@@ -11,10 +12,10 @@ fn count_digits(stone: &usize) -> usize {
 
 fn split_digits(stone: &usize) -> Vec<usize> {
     let digits = count_digits(stone) as u32;
-    let factor = 10usize.pow(digits / 2) as u32;
+    let factor = 10usize.pow(digits / 2) as u64;
     vec![
-        (*stone as u32 / factor) as usize,
-        (*stone as u32 % factor) as usize,
+        (*stone as u64 / factor) as usize,
+        (*stone as u64 % factor) as usize,
     ]
 }
 
@@ -41,6 +42,34 @@ fn transform_n(stones: &Vec<usize>, n: usize) -> Vec<usize> {
     current
 }
 
+fn transform2(stones: &HashMap<usize, usize>) -> HashMap<usize, usize> {
+    let mut new_stones: HashMap<usize, usize> = HashMap::new();
+    for (stone, count) in stones {
+        if *stone == 0 {
+            *new_stones.entry(1).or_insert(0) += count;
+        } else if count_digits(stone) % 2 == 0 {
+            let new_digits = split_digits(stone);
+            *new_stones.entry(new_digits[0]).or_insert(0) += count;
+            *new_stones.entry(new_digits[1]).or_insert(0) += count;
+        } else {
+            *new_stones.entry(stone * 2024).or_insert(0) += count;
+        }
+    }
+    new_stones
+}
+
+fn transform_n2(stones: &Vec<usize>, n: usize) -> HashMap<usize, usize> {
+    let mut current: HashMap<usize, usize> = HashMap::new();
+    for stone in stones {
+        *current.entry(*stone).or_insert(0) += 1
+    }
+
+    for _ in 0..n {
+        current = transform2(&current)
+    }
+    current
+}
+
 fn main() {
     let raw_input = fs::read_to_string(env::args().nth(1).expect("Missing filename input"))
         .expect("Failed to read input");
@@ -51,10 +80,14 @@ fn main() {
         .map(|s| s.parse().expect("Failed to parse"))
         .collect();
 
-    println!("Initial stones: {:?}", input);
+    //println!("Initial stones: {:?}", input);
 
-    for steps in [1, 5, 25] {
-        let stones = transform_n(&input, steps);
-        println!("Stones after {} steps is {}: {:?}", steps, stones.len(), stones.iter().take(10).collect::<Vec<&usize>>());
+    // Part 1 was initially completed with `transform_n`, later replaced with `transform_n2`.
+    for steps in [25, 75] {
+        //let stones = transform_n(&input, steps);
+        //println!("Stones after {} steps is {}: {:?}", steps, stones.len(), stones);
+        let stones = transform_n2(&input, steps);
+        let total = stones.iter().map(|(_, count)| count).fold(0, |acc, el| acc + *el);
+        println!("Stones after {} steps is {:?}", steps, total);
     }
 }
